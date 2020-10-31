e84fdafe5315865f1ab15c79c6be9697b6596d33
Gia Jennings <gjennings.co@gmail.com>
2020-10-31 16:43:24 -0400

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
+    array.include?(element)
 end
 
 def using_sort(array)
-
+    array.sort!
 end
 
-def using_reverse(array)
-
+def using_reverse(wow)
+    wow.reverse
 end
 
-def using_first(array)
-
+def using_first(wow)
+    wow.first
 end
 
 def using_last(array)
-
+    array.last
 end
 
-def using_size(array)
-
+def using_size(array = "lemon," "lime," "apple," "plum," "peach," "banana")
+    array.size
 end
